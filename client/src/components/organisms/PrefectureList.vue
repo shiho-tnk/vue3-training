eb997f8a28a156502cc578cef7a9a13ee3a586db
田中 志帆 <tanaka-shiho@ksc365.onmicrosoft.com>
2023-09-19 16:45:12 +0900

    チェックボックスの実装、v-ifの活用

--- client/src/components/organisms/PrefectureList.vue
+++ client/src/components/organisms/PrefectureList.vue
@@ -2,8 +2,20 @@
 import { onMounted, ref } from "vue"
 import axiosInstance from "@/utils/axiosSettings"
 import { PrefectureDisplay } from "@/types/prefecture"
+
+// const prefecture1 = ref("千葉県")
+
 // TODO: prefectureで千葉県を表示してみましょう
-const prefecture = ref<PrefectureDisplay>({})
+const prefecture = ref<PrefectureDisplay>({
+  prefName: "千葉県",
+  prefNo: 1,
+  isCheck: false
+})
+console.log(prefecture.value.isCheck)
+
+function click(bool: boolean) {
+  prefecture.value.isCheck = bool
+}
 
 onMounted(() => {
   // TODO: 全県取得のAPIへリクエストを送ってみましょう!
@@ -13,6 +25,11 @@ onMounted(() => {
 <template>
   <div class="prefecture-container">
     <h3>都道府県</h3>
+    <div>
+      <input type="checkbox" @click="click(!prefecture.isCheck)" />
+      <label>千葉県</label>
+    </div>
+    <div v-if="prefecture.isCheck">true</div>
     <div class="prefecture-flex">
       <!-- TODO: 県を表示してみましょう -->
     </div>
